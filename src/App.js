8948c5da92c3e15648f2cda59fe05502bf2d830c
Lucas Martinez LLosco <linkgz55@gmail.com>
2022-09-26 22:52:03 -0300

    a trabajar

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import '../src/componentes/css/App.css';
+
+function App() {
+    return (
+        <>
+              {/* <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Home/>}></Route>
+                        <Route path="jugar" element={<Juego/>}></Route>
+                        <Route path="info" element={ <Conocenos/> }></Route>
+                    </Routes>
+                </BrowserRouter> */}
+        </>
+    
+           
+    );
+
+}
+
+export default App;
